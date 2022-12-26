e196d75c6e43bfa88cd5ffa97c6986cc99149f41
albertoalonsofernandez <berto_93_@hotmail.com>
2022-12-26 12:52:23 +0100

    add User

---
+++ models/User.js
@@ -0,0 +1,40 @@
+import bcryptjs from "bcryptjs";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        index: { unique: true },
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+
+    if (!user.isModified("password")) return next();
+
+    try {
+        const salt = await bcryptjs.genSalt(10);
+
+        user.password = await bcryptjs.hash(user.password, salt);
+
+        next();
+    } catch (e) {
+        console.log(e);
+        throw new Error("Falló el hash de contraseña");
+    }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcryptjs.compare(candidatePassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
